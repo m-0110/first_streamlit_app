22311c0e0a7ec1de6d0665ba3a174cea608dc033
m-0110 <63664340+m-0110@users.noreply.github.com>
2023-09-28 19:28:16 +0530

    Update streamlit_app.py
    added text input box and pass it to api

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,10 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #New section
 streamlit.header('Fruityvice Fruit Advice')
+fruit_choice = streamlit.text_input("What fruit would you like information about?","Kiwi")#"Kiwi default input
+streamlit.write('The user entered ', fruit_choice)
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 #normalize json
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
