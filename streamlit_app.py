ff6bde590b7e839e6a6061ff326f7b3d5cc3034b
m-0110 <63664340+m-0110@users.noreply.github.com>
2023-10-02 22:13:44 +0530

    Update streamlit_app.py
    added insert stat

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,7 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row1)
 
 streamlit.header('Fruityvice Fruit Advice')
-fruit_choice1 = streamlit.text_input("What fruit you would like to add?","Kiwi")#"Kiwi default input
+fruit_choice1 = streamlit.text_input("What fruit you would like to add?","jackfruit")#"Kiwi default input
 streamlit.write('Thanks for adding ', fruit_choice1)
+my_cur.execute("insert into fruit_load_list values(fruit_choice1))
 
