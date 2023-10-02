d2360b2ad34374b67af3f42b20917f909a557779
m-0110 <63664340+m-0110@users.noreply.github.com>
2023-10-02 22:35:28 +0530

    Update streamlit_app.py
    cleaned code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -9,6 +11,7 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Bolied Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
@@ -18,26 +21,12 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice')
 fruit_choice = streamlit.text_input("What fruit would you like information about?","Kiwi")#"Kiwi default input
 streamlit.write('The user entered ', fruit_choice)
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row1 = my_cur.fetchall()
 #normalize json
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 #make sure this streamlit.dataframe is at end
 streamlit.dataframe(fruityvice_normalized)
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row1)
 
-streamlit.header('Fruityvice Fruit Advice')
-fruit_choice1 = streamlit.text_input("What fruit you would like to add?","jackfruit")#"Kiwi default input
-streamlit.write('Thanks for adding ', fruit_choice1)
-my_cur.execute("insert into fruit_load_list values('{}')".format(fruit_choice1))
 
