ee80af56338e6c79a411e93a0dc1c207033097f4
m-0110 <63664340+m-0110@users.noreply.github.com>
2023-10-02 22:03:06 +0530

    Update streamlit_app.py
    added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,11 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
-streamlit.dataframe(my_data_row)
+streamlit.text(my_data_row)
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row1 = my_cur.fetchone()
-streamlit.text(my_data_row1)
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row1)
 #normalize json
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 #make sure this streamlit.dataframe is at end
